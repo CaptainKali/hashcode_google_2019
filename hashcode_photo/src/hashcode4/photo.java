156996cab84ded7943196c88623017ed2da7e19b
Dylan <dylanliner@gmail.com>
2019-02-28 19:42:00 +0100

    classe photo

---
+++ hashcode_photo/src/hashcode4/photo.java
@@ -0,0 +1,40 @@
+package hashcode4;
+
+import java.util.ArrayList;
+
+public class photo {
+	
+	private String orientation;
+	
+	private int nbTags;
+	
+	private ArrayList<String> tags = new ArrayList<String>();
+
+	public String getOrientation() {
+		return orientation;
+	}
+
+	public void setOrientation(String orientation) {
+		this.orientation = orientation;
+	}
+
+	public int getNbTags() {
+		return nbTags;
+	}
+
+	public void setNbTags(int nbTags) {
+		this.nbTags = nbTags;
+	}
+
+	public ArrayList<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(ArrayList<String> tags) {
+		this.tags = tags;
+	}
+	
+	
+	
+
+}
