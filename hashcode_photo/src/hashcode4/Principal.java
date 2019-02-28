b2e1dadc493247166b6a982997d28de746293503
Dylan <dylanliner@gmail.com>
2019-02-28 19:36:01 +0100

    test

--- hashcode_photo/src/hashcode4/Principal.java
+++ hashcode_photo/src/hashcode4/Principal.java
@@ -4,7 +4,14 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		 
 
+	}
+	
+	private void sysout() {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
 	}
 
 }
