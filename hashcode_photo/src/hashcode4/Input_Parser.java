55191c28393a5c9e1682bd0c1e4255b9665f9697
DE FIGUEIREDO Clement <clement.defigueiredo@soprasteria.com>
2019-02-28 19:48:34 +0100

    add input parser

---
+++ hashcode_photo/src/hashcode4/Input_Parser.java
@@ -0,0 +1,19 @@
+package hashcode4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputParser
+
+    public list <Photo> parse(String filename) {
+        AtomicInteger id = new AtomicInteger() 
+        return new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(filename))).lines()
+        .skip(1)
+        .map -> line 
+    }
--- hashcode_photo/src/hashcode4/utils.java
+++
@@ -1,1 +0,0 @@
-package hashcode4;
